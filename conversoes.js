18689a0b3e74e0e61303abff35d8ad4665d552c5
RMartins <freitas.rodrigom@gmail.com>
2023-07-14 13:53:59 -0300

    praticando conversoes number e string

---
+++ conversoes.js
@@ -0,0 +1,21 @@
+// tipo de dado
+// booleans
+
+// conversao implicita
+
+const numero = 456;
+const numeroString = '456';
+
+console.log(numero === numeroString)
+console.log(numero == numeroString)
+console.log(numero + numeroString)
+
+// conversao explicita
+
+console.log(numero === Number(numeroString))
+console.log(numero ==  Number(numeroString))
+console.log(numero +  Number(numeroString))
+
+console.log(String(numero) === numeroString)
+console.log(String(numero) ==  numeroString)
+console.log(String(numero) +  numeroString)
