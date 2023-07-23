6557fe55447a04505e8c001f7120a9f0fd4429a6
RMartins <freitas.rodrigom@gmail.com>
2023-07-23 19:26:54 -0300

    criando um template string com acento grave e

---
+++ template.js
@@ -0,0 +1,14 @@
+// const nome = "Ju";
+// const apresentacao = "meu nome é " + nome;
+// console.log(apresentacao)
+
+const nome = "Ju";
+const idade = 2021-1981;
+const cidadeNascimento = "São Paulo";
+
+// const apresentacao = "meu nome é " + nome + ", a minha idade é " + iodade + " e nasci na cidade de "+ cidadeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacao)
+
