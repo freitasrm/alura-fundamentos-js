954c26451633f878c811ced3a8b4cd3b0ab9876a
RMartins <freitas.rodrigom@gmail.com>
2023-07-23 17:53:07 -0300

    update projeto curso Js Erros e console

---
+++ console.js
@@ -0,0 +1,16 @@
+// log -> registro
+
+const minharVar = true;
+
+console.log(245)
+console.log("eu sou um texto")
+console.log(minharVar)
+
+// tratamento de erro!
+
+console.error('deu erro!')
+console.log("deu erro");
+console.error("deu erro");
+
+console.log("deu erro");
+console.error(new Error("deu erro"));
---
+++ erros.js
@@ -0,0 +1,4 @@
+const numero = 1;
+console.log(numero);
+// const minharvar = "oi";
+console.log(minharvar)
