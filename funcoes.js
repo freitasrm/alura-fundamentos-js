20503031f920ddd82fc96c52c44f468b832e82f4
RMartins <freitas.rodrigom@gmail.com>
2023-07-23 19:54:16 -0300

    inserindo funções js

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) declara a função
+                    //string
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto("oi");
+imprimeTexto("outro texto");
+
+// três formas de escrever funções
+
+function soma() {
+    // outros códigos antes de ter o return
+    // const resultado = 2 + 2;
+    return 2 + 2
+}
+
+console.log(soma())
+imprimeTexto(soma())
