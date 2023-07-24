780f81e2a6bf5664bc3feff227cf212f2243c41e
RMartins <freitas.rodrigom@gmail.com>
2023-07-24 09:21:38 -0300

    teste de declaração de arrays e arrays vazios

---
+++ index.js
@@ -0,0 +1,16 @@
+//const arrayVazia = [];
+const arrayVazia = [,,,];
+
+console.log(arrayVazia)
+console.log(arrayVazia.length)
+console.log(arrayVazia[0])
+
+console.log(arrayVazia.length)
+console.log(arrayVazia[0])
+console.log(arrayVazia[1])
+console.log(arrayVazia[2])
+
+console.log(arrayVazia.length)
+arrayVazia.push(50)
+console.log(arrayVazia)
+console.log(arrayVazia.length)
