999a88f4654f505f7037db65a22f7926fcb35f7e
RMartins <freitas.rodrigom@gmail.com>
2023-07-24 09:53:52 -0300

    concatenando listas

---
+++ juntando-salas.js
@@ -0,0 +1,8 @@
+const salaJS = ['Evaldo', 'Camis', 'Mari'];
+const salaPython = ['Ju', 'Leo', 'Raquel'];
+
+const salasUnificadas = salaJS.concat(salaPython);
+const salasUnificadas2 = salaPython.concat(salaJS);
+
+console.log(salasUnificadas);
+console.log(salasUnificadas2);
