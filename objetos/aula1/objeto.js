6fd8f75ae912450005ab516b4a7da3115b763606
RMartins <freitas.rodrigom@gmail.com>
2023-07-25 11:11:46 -0300

    organizando a pasta dos cursos

---
+++ objetos/aula1/objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = [1111111, 222222, 33333];
+
+const informacoesPessoa = ["nome", "jose", "idade", 32, "cPF", "1111111"];
+
+console.log(informacoesPessoa[1])
+ 
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32,
+};
