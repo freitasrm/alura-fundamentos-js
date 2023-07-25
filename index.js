57d2bd40d7815987442e95eeadbc563618682fe4
RMartins <freitas.rodrigom@gmail.com>
2023-07-25 09:43:08 -0300

    explorando map com alteração de nome para uppercase

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ console.log(arrayVazia.length)
 arrayVazia.push(50)
 console.log(arrayVazia)
 console.log(arrayVazia.length)
+
+
+// TESTE FUNÇÕES CALLBACK
+const arrayNums = [1, 2, 3, 4]
+
+
+// // FUNÇÃO CRIADA PARA REPRESENTAR O QUE PODE SER FEITO NO MAP POR CALLBACK
+// function multiplicaPorDez(num) {
+//     return num * 10
+// }
+// ///
+// const arraySomada = arrayNums.map(multiplicaPorDez)
+
+// UTILIZANDO ARROW FUNCTION ANÔNIMA
+const arraySomada = arrayNums.map(num => num * 10);
+
+console.log(arraySomada)
---
+++ map-nomes.js
@@ -0,0 +1,10 @@
+const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva'];
+
+// const nomesPadronizados = nomes.map((nome) => {
+//     return nome.toUpperCase();
+// })
+
+// forma consisa
+const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesPadronizados);
