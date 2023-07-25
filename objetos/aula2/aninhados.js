969d63a66a641af4ea614881cb42037800440480
RMartins <freitas.rodrigom@gmail.com>
2023-07-25 14:30:47 -0300

    aninhando objetos em outros objetos

---
+++ objetos/aula2/aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["11333333000", "12221312312"]
+};
+
+cliente.endereco = {
+    rua: "R. Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+};
+
+console.log(cliente);
