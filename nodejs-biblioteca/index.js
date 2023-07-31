7184883040c44e25335a1215d783a41a6e1985d1
RMartins <freitas.rodrigom@gmail.com>
2023-07-31 11:24:34 -0300

    capturando o texto do arquivo md com regex

--- nodejs-biblioteca/index.js
+++ nodejs-biblioteca/index.js
@@ -5,16 +5,19 @@ const log = console.log;
 const aviso = chalk.bgRed.bold;
 const padrao = chalk.greenBright.bgWhite.bold;
 
-const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+// const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
 
 function extraiLinks(texto) {
     const regex = /\[([^\[\]]*?)\]\((https?:\/\/[^\s?#.]*.[^\s]*)\)/gm;
     // const capturas = texto.match(regex)
-    const capturas = regex.exec(texto)
-    log(capturas)
+    // const capturas = regex.exec(texto)
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    // log(resultados)
+    return resultados;
 }
 
-extraiLinks(textoTeste)
+// extraiLinks(textoTeste)
 
 function trataErro(erro) {
     // console.log(erro);
@@ -25,9 +28,10 @@ function trataErro(erro) {
 
 async function pegaArquivo(caminhoDoArquivo) {
     try {
-        const encoding = 'utf-8'
+        const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        log(aviso(texto))
+        // log(aviso(texto))
+        log(extraiLinks(texto));
     } catch (erro) {
         trataErro(erro)
     } finally {
@@ -56,7 +60,7 @@ async function pegaArquivo(caminhoDoArquivo) {
 //     })
 // }
 
-// pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/texto.md');
 
 // pegaArquivo('./arquivos/');
 
