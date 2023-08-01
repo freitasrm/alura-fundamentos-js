69f3fe4915a055e718bd2d134e0e6913c7656db9
RMartins <freitas.rodrigom@gmail.com>
2023-08-01 17:26:37 -0300

    encapsulando os atributos da classe user

--- orientacao-objetos/index.js
+++ orientacao-objetos/index.js
@@ -4,3 +4,8 @@ import Admin from "./Admin.js";
 
 const novoUser = new User('Mariana', 'm@m.com', '202-01-01');
 console.log(novoUser.exibirInfos());
+
+novoUser.nome = 'Márcia';
+console.log(novoUser.nome)
+console.log(novoUser)
+console.log(novoUser.exibirInfos());
