4b7de5e2682914a15e48e1d0c1a90af83620c7ea
RMartins <freitas.rodrigom@gmail.com>
2023-07-31 21:39:46 -0300

    finalizando o projeto com a validação dos status e tratamento de erro de status dos links validados

--- nodejs-biblioteca/src/http-validacao.js
+++ nodejs-biblioteca/src/http-validacao.js
@@ -1,28 +1,40 @@
+import chalk from "chalk";
+
 function extraiLnks(arrLinks) {
     //loop
     return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
 
 async function checaStatus(listaURLs) {
-    const arrStatus = await Promise.all(
+    const arrStatus = await Promise
+    .all(
         listaURLs.map( async (url) => {
-        const response = await fetch(url);
-        return response.status;
+            try {
+                const response = await fetch(url);
+                // return response.status;
+                return `${response.status} - ${response.statusText}`;
+            } catch (erro) {
+                return manejaErros(erro);
+            }
         })
     )    
     return arrStatus;
 }
 
+function manejaErros(erro) {
+    if (erro.cause.code === 'ENOTFOUND') {
+        return 'link não encontrado';
+    } else {
+        return 'ocorreu algum erro';
+    }
+}
+
 export default async function listaValidada(listaDeLinks) {
     const links = extraiLnks(listaDeLinks);
     const status = await checaStatus(links);
-    return status;
+
+    return listaDeLinks.map((objeto, indice) => ({
+        ...objeto,
+        status: status[indice]
+    }))
 }
-
-// const res = await fetch('https://nodejs.org/api/documentation.json');
-// if (res.ok) {
-//     const data = await res.json();
-//     console.log(data);
-// }
-
-//[gatinho salsicha](http://gatinhosalsicha.com.br/)
