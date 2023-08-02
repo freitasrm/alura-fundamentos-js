0bfc14e823a7830c345dd2a666430091f53abf15
RMartins <freitas.rodrigom@gmail.com>
2023-08-02 10:21:32 -0300

    trabalhando com polimorfismo em javascript

--- orientacao-objetos/index.js
+++ orientacao-objetos/index.js
@@ -11,16 +11,7 @@ const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
 console.log(novoAdmin.nome)
 novoAdmin.nome = 'Reinaldo'
 console.log(novoAdmin.nome)
-console.log(novoAdmin)
 
-const string1 = 'Abel'
-const string2 = 'Calhim'
-const string4 = 'Romario Amazonense dos Santos'
-const string3 = [string1, ...string2]
-const [string5, string6, ...string7] = string4.split(" ")
-console.log('string3',string3)
-console.log('string4',string4)
-console.log('string5',string5)
-console.log('string6',string6)
-console.log('string6',string7)
-console.log('string7', string7.join(" "))
+const novoDocente = new Docente('Guilherme', 'g@g.com', '2021-01-01');
+console.log(novoDocente)
+console.log(novoDocente.exibirInfos())
