4f6b75b890ac074ef1ecff961d846c18aa790262
RMartins <freitas.rodrigom@gmail.com>
2023-08-05 14:07:48 -0300

    aprimorando o projeto no padrão de mercado com express, substituindo o http do javascript

--- nodejs-api/server.js
+++ nodejs-api/server.js
@@ -1,19 +1,29 @@
-const http = require("http")
-const port = 3000;
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na pag de Livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Pag de editora',
-    '/sobre': 'Info sobre projeto'
-}
+// CRIANDO SERVER COM HTTP
+// const http = require("http")
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(rotas[req.url]);
-})
+// const rotas = {
+//     '/': 'Curso de Node',
+//     '/livros': 'Entrei na pag de Livros',
+//     '/autores': 'Listagem de autores',
+//     '/editora': 'Pag de editora',
+//     '/sobre': 'Info sobre projeto'
+// }
 
-server.listen(port, () => {
+// const server = http.createServer((req, res) => {
+    //     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //     res.end(rotas[req.url]);
+    // })
+    
+    // server.listen(port, () => {
+        //     console.log(`Servidor escutando em http://localhost:${port}`)
+        // })
+
+// CRIANDO SERVER COM EXPRESS
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
