e54974a4867385370a4092b3f5c79419065d9b2c
RMartins <freitas.rodrigom@gmail.com>
2023-08-05 18:09:32 -0300

    configurando um schema do mongoDb ao projeto

--- nodejs-api/src/app.js
+++ nodejs-api/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
 
@@ -11,10 +12,10 @@ const app = express();
 
 app.use(express.json())
 
-const livros = [
-    { id: 1, "titulo": "Senhor dos Aneis" },
-    { id: 2, "titulo": "O Hobiit" },
-]
+// const livros = [
+//     { id: 1, "titulo": "Senhor dos Aneis" },
+//     { id: 2, "titulo": "O Hobiit" },
+// ]
 
 app.post('/livros', (req, res) => {
     livros.push(req.body);
@@ -26,7 +27,9 @@ app.get('/', (req, res) => {
 })
 
 app.get('/livros', (req, res) => {
-    res.status(200).json(livros)
+    livros.find((err, livros) => {
+        res.status(200).json(livros)
+    })
 })
 
 app.get('/livros/:id', (req, res) => {
