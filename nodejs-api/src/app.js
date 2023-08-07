a79beef3de82c3cc90253b3c483b4ad4f1e3b6fd
RMartins <freitas.rodrigom@gmail.com>
2023-08-07 16:17:29 -0300

    refatorando o app.js sem os códigos depreciados

--- nodejs-api/src/app.js
+++ nodejs-api/src/app.js
@@ -15,46 +15,4 @@ app.use(express.json())
 
 routes(app);
 
-// const livros = [
-//     { id: 1, "titulo": "Senhor dos Aneis" },
-//     { id: 2, "titulo": "O Hobiit" },
-// ]
-
-// app.post('/livros', (req, res) => {
-//     livros.push(req.body);
-//     res.status(201).send('Livro foi cadastrado com sucesso')
-// })
-
-// app.get('/', (req, res) => {
-//     res.status(200).send('Curso de Node');
-// })
-
-// app.get('/livros', (req, res) => {
-//     livros.find((err, livros) => {
-//         res.status(200).json(livros)
-//     })
-// })
-
-// app.get('/livros/:id', (req, res) => {
-//     let index = buscaLivro(req.params.id);
-//     res.json(livros[index]);
-// })
-
-// app.put('/livros/:id', (req, res) => {
-//     let index = buscaLivro(req.params.id);
-//     livros[index].titulo = req.body.titulo;
-//     res.json(livros);
-// })
-
-// app.delete('/livros/:id', (req, res) => {
-//     let { id } = req.params;
-//     let index = buscaLivro(id);
-//     livros.splice(index, 1);
-//     res.send(`Livro ${id} removido com sucesso`);
-// })
-
-// function buscaLivro(id) {
-//     return livros.findIndex(livros => livros.id == id);
-// }
-
 export default app
