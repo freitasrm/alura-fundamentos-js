e7f28c59eec04bb7b815c8292a566f85995038b1
RMartins <freitas.rodrigom@gmail.com>
2023-08-07 16:16:51 -0300

    finalizando o crud com controllers e routers

--- nodejs-api/src/app.js
+++ nodejs-api/src/app.js
@@ -35,26 +35,26 @@ routes(app);
 //     })
 // })
 
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-})
+// app.get('/livros/:id', (req, res) => {
+//     let index = buscaLivro(req.params.id);
+//     res.json(livros[index]);
+// })
 
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
-})
+// app.put('/livros/:id', (req, res) => {
+//     let index = buscaLivro(req.params.id);
+//     livros[index].titulo = req.body.titulo;
+//     res.json(livros);
+// })
 
-app.delete('/livros/:id', (req, res) => {
-    let { id } = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro ${id} removido com sucesso`);
-})
+// app.delete('/livros/:id', (req, res) => {
+//     let { id } = req.params;
+//     let index = buscaLivro(id);
+//     livros.splice(index, 1);
+//     res.send(`Livro ${id} removido com sucesso`);
+// })
 
-function buscaLivro(id) {
-    return livros.findIndex(livros => livros.id == id);
-}
+// function buscaLivro(id) {
+//     return livros.findIndex(livros => livros.id == id);
+// }
 
 export default app
