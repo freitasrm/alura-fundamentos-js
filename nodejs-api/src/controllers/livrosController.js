63802d250534b60cae0233c3b9838dcd9bee7227
RMartins <freitas.rodrigom@gmail.com>
2023-08-07 22:45:28 -0300

    implementando a busca por editora e fazendo join de autores com livros

--- nodejs-api/src/controllers/livrosController.js
+++ nodejs-api/src/controllers/livrosController.js
@@ -3,9 +3,11 @@ import livros from "../models/Livro.js";
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            res.status(200).json(livros)
-        })
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
+                res.status(200).json(livros)
+            })
     }
 
     static listarLivroPorId = (req, res) => {
@@ -54,6 +56,14 @@ class LivroController {
             } else {
                 res.status(500).send({message: err.message})
             }
+        })
+    }
+
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora;
+
+        livros.find({ 'editora': editora }, {}, (err, livros) => {
+            res.status(200).send(livros);
         })
     }
 
